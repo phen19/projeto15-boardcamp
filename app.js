82e3f88cc640b2463c1c094fb4a2393763dfda7f
ph-ubuntu-pc <phenriquelgsilva@gmail.com>
2022-07-22 21:36:21 -0300

    POST and GET games implemented

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const categoriesSchema = joi.object( {name: joi.string().required()})
+
 
 
 app.get('/categories', async (req, res) => {
@@ -27,7 +27,7 @@ app.get('/categories', async (req, res) => {
 
 app.post('/categories', async (req, res) => {
     try{
-    
+    const categoriesSchema = joi.object( {name: joi.string().required()})
     const name = req.body;
     const validation = categoriesSchema.validate(name,{abortEarly: false});
     if(validation.error){
@@ -43,9 +43,6 @@ app.post('/categories', async (req, res) => {
           }
 
     await connection.query('INSERT INTO categories (name) VALUES ($1)', [name.name])
-    
-
-    
 
     res.sendStatus(201)
     } catch (err){
@@ -56,6 +53,59 @@ app.post('/categories', async (req, res) => {
 
 })
 
+
+app.post('/games', async(req, res) => {
+    try{
+    const gamesSchema = joi.object({
+        name: joi.string().required(),
+        image: joi.string().required(),
+        stockTotal: joi.number().min(1).required(),
+        categoryId: joi.number().required(),
+        pricePerDay: joi.number().min(1).required()
+    })
+
+    const game = req.body
+    const validation = gamesSchema.validate(game,{abortEarly: false});
+    if(validation.error){
+        console.log(validation.error)
+        res.status(400).send(validation.error.details.map(item => item.message))
+        return
+    }
+
+    const existingGame = await connection.query('SELECT * FROM games WHERE name = $1', [game.name])
+        console.log(existingGame)
+        if (existingGame.rowCount !== 0) {
+            return res.sendStatus(409);
+          }
+    
+    await connection.query(`
+    INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") 
+    VALUES ($1, $2, $3, $4, $5)`, [game.name, game.image, game.stockTotal, game.categoryId, game.pricePerDay])
+
+    res.sendStatus(201)
+    
+    }catch (err){
+        console.error(err);
+        res.sendStatus(500);
+    }
+
+
+})
+
+app.get('/games', async (req, res) => {
+    try{
+     console.log(connection)
+     const games = await connection.query('SELECT * FROM games;')
+     
+     res.send(games.rows);
+ 
+    } catch (err){
+     console.error(err);
+     res.sendStatus(500);
+    }
+     
+   });
+ 
 
 app.listen(4000, () => {
     console.log('Server listening on port 4000.');
